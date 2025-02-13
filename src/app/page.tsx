ecc60d4081388a28e43cc9a102819616cde11b81
OmegazonCMoi <fabroc300@gmail.com>
2025-02-13 09:40:17 +0100

    ESLint Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,12 @@ import { Lato } from 'next/font/google'
 import Image from "next/image";
 
 const latoThin = Lato({
+  subsets: ["latin"],
   weight: '100'
 });
 
 const latoNormal = Lato({
+  subsets: ["latin"],
   weight: '300'
 });
 
@@ -22,7 +24,7 @@ export default function Home() {
           <h1 className={`${latoThin.className} text-6xl`}>Forum Cybersécurité 2025</h1>
           <div className="mt-6 space-x-2">
             <a href="#" className={`${latoNormal.className} px-6 py-3 bg-neutral-800 text-neutral-100 rounded-[0.5em] hover:shadow-lg transition-all duration-300`}>Découvrir</a>
-            <a href="#" className={`${latoNormal.className} px-6 py-3 border border-neutral-800 text-neutral-800 rounded-[0.5em] hover:shadow-lg transition-all duration-300`}>S'inscrire</a>
+            <a href="#" className={`${latoNormal.className} px-6 py-3 border border-neutral-800 text-neutral-800 rounded-[0.5em] hover:shadow-lg transition-all duration-300`}>S&apos;inscrire</a>
           </div>
         </div>
         <Image src='/images/security-illustration.svg' alt='Security Illustration' className="mr-10" width={350} height={350} />
