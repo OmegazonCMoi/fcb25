7628eb2e54bc43ffd1ae576fe45060bc1a341eb8
OmegazonCMoi <fabroc300@gmail.com>
2025-03-12 22:25:48 +0100

    Ateliers Finis

--- src/data/ateliers.tsx
+++ src/data/ateliers.tsx
@@ -1,9 +1,19 @@
-import { Battery, Dock, Grab, LogOut, Pyramid, Radiation, Salad, Usb, Wheat, Wifi } from "lucide-react";
+import {
+    Bitcoin,
+    DoorOpen,
+    Eye,
+    Fish,
+    Lock,
+    Server,
+    Usb,
+    Wheat,
+    Wifi
+} from "lucide-react";
 
 export const ateliers = [
     {
         id: 1,
-        title: "Atelier WiFi Public",
+        title: "Atelier MITM",
         description: "Tentez de remporter un séjour pour 2 personnes dans un hôtel en Suisse grâce à ce concours Instagram !",
         hotes: ["Zitoune", "Pignouf"],
         icon: <Wifi className="size-4" strokeWidth={1} />,
@@ -11,7 +21,7 @@ export const ateliers = [
     },
     {
         id: 2,
-        title: "Atelier N°2",
+        title: "Atelier Rubber Ducky",
         description: "Un simple clic et vos photos privées se retrouvent sur le web. Toujours sûr de vouloir brancher cette clé USB ?",
         hotes: ["Zitoune", "Pignouf"],
         icon: <Usb className="size-4" strokeWidth={1} />,
@@ -19,64 +29,64 @@ export const ateliers = [
     },
     {
         id: 3,
-        title: "Atelier N°3",
-        description: "Oui bonjour j'aime les quiches ah bah non en fait c'est pas si ouf en vrai.",
+        title: "Atelier Mot de Passe",
+        description: "Votre mot de passe est-il vraiment à l’épreuve des hackers ? Venez le découvrir… avant qu’il ne soit trop  tard !",
         hotes: ["Zitoune", "Pignouf"],
-        icon: <Battery className="size-4" strokeWidth={1} />,
+        icon: <Lock className="size-4" strokeWidth={1} />,
         imgsrc: "/images/ateliers/3.png"
     },
     {
         id: 4,
-        title: "Atelier N°4",
-        description: "Oui bonjour j'aime les quiches ah bah non en fait c'est pas si ouf en vrai.",
+        title: "Atelier Phishing",
+        description: "Et si ce message super urgent ou cette promo de rêve était en fait… un piège ? Venez apprendre à ne plus se faire avoir !",
         hotes: ["Zitoune", "Pignouf"],
-        icon: <Dock className="size-4" strokeWidth={1} />,
-        imgsrc: "/images/ateliers/4.png"
-    },
-    {
-        id: 5,
-        title: "Atelier N°5",
-        description: "Oui bonjour j'aime les quiches ah bah non en fait c'est pas si ouf en vrai.",
-        hotes: ["Zitoune", "Pignouf"],
-        icon: <Grab className="size-4" strokeWidth={1} />,
+        icon: <Fish className="size-4" strokeWidth={1} />,
         imgsrc: "/images/ateliers/5.png"
     },
     {
-        id: 6,
-        title: "Atelier N°6",
-        description: "Oui bonjour j'aime les quiches ah bah non en fait c'est pas si ouf en vrai.",
+        id: 5,
+        title: "Atelier Backdoor",
+        description: "Imaginez qu’on entre chez vous sans jamais passer par la porte… Sur votre ordinateur, c’est aussi possible.",
         hotes: ["Zitoune", "Pignouf"],
-        icon: <LogOut className="size-4" strokeWidth={1} />,
+        icon: <DoorOpen className="size-4" strokeWidth={1} />,
         imgsrc: "/images/ateliers/6.png"
     },
     {
-        id: 7,
-        title: "Atelier N°7",
-        description: "Oui bonjour j'aime les quiches ah bah non en fait c'est pas si ouf en vrai.",
+        id: 6,
+        title: "Atelier DoS",
+        description: "Que feriez-vous si, en quelques secondes, tout votre système se retrouvait complètement paralysé ?",
         hotes: ["Zitoune", "Pignouf"],
-        icon: <Radiation className="size-4" strokeWidth={1} />,
+        icon: <Server className="size-4" strokeWidth={1} />,
         imgsrc: "/images/ateliers/1.png"
     },
     {
-        id: 8,
-        title: "Atelier N°8",
-        description: "Oui bonjour j'aime les quiches ah bah non en fait c'est pas si ouf en vrai.",
+        id: 7,
+        title: "Atelier Ransomware",
+        description: "Un matin, vous allumez votre ordinateur et… tous vos fichiers sont bloqués. Payer ou tout perdre ? Apprenez à éviter ce choix impossible.",
         hotes: ["Zitoune", "Pignouf"],
-        icon: <Pyramid className="size-4" strokeWidth={1} />,
+        icon: <Bitcoin className="size-4" strokeWidth={1} />,
         imgsrc: "/images/ateliers/2.png"
     },
     {
-        id: 9,
-        title: "Atelier N°9",
-        description: "Oui bonjour j'aime les quiches ah bah non en fait c'est pas si ouf en vrai.",
+        id: 8,
+        title: "Atelier OSINT",
+        description: "Vous seriez surpris de tout ce qu’on peut savoir sur vous… Découvrez comment les hackers prennent le contrôle de votre vie privée.",
         hotes: ["Zitoune", "Pignouf"],
-        icon: <Salad className="size-4" strokeWidth={1} />,
+        icon: <Eye className="size-4" strokeWidth={1} />,
         imgsrc: "/images/ateliers/3.png"
     },
     {
+        id: 9,
+        title: "Atelier Dark Web",
+        description: "Le Dark Web : mythe ou réalité ? Plongez dans les profondeurs d'Internet et découvrez ce qui s'y cache réellement, entre mystères et dangers.",
+        hotes: ["Zitoune", "Pignouf"],
+        icon: <Wheat className="size-4" strokeWidth={1} />,
+        imgsrc: "/images/ateliers/4.png"
+    },
+    {
         id: 10,
-        title: "Atelier N°10",
-        description: "Oui bonjour j'aime les quiches ah bah non en fait c'est pas si ouf en vrai.",
+        title: "Atelier IA",
+        description: "L’intelligence artificielle est une alliée… jusqu’au jour où elle devient une menace.",
         hotes: ["Zitoune", "Pignouf"],
         icon: <Wheat className="size-4" strokeWidth={1} />,
         imgsrc: "/images/ateliers/4.png"
