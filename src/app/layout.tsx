3a747ec550b0cbd894c07387adad78bcf683e055
OmegazonCMoi <fabroc300@gmail.com>
2025-03-30 13:45:30 +0200

    J'AI PEUT ETRE UNE ALTERNANCE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,21 +10,12 @@ export const metadata: Metadata = {
     description: "Forum Cybersécurité 2025, un pour découvrir différentes techniques de hacking et de sécurité informatique.",
     url: "https://forum-cybersecurite.netlify.app/",
     type: "website",
-    images: [
-      {
-        url: "https://i.imgflip.com/77uo3z.jpg?a483048",
-        width: 1200,
-        height: 630,
-        alt: "Silly cat :3",
-      },
-    ],
     siteName: "Forum Cybersécurité 2025",
   },
   twitter: {
     card: "summary_large_image",
     title: "Forum Cybersécurité 2025",
     description: "Forum Cybersécurité 2025, un pour découvrir différentes techniques de hacking et de sécurité informatique.",
-    images: ["https://i.imgflip.com/77uo3z.jpg?a483048"],
   },
   icons: {
     icon: "/favicon.ico",
